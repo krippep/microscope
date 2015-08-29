07a3a097073fbb39eaf140afc612e78456851b5e
krippep <kristofferp.91@gmail.com>
2015-08-29 17:07:32 +0200

    adding some JS magic to posts

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,21 @@
+var postsData = [
+	{
+		title: 'Malmö FF',
+		url: 'http://www.mff.se'
+	},
+	{
+		title: 'Sydskånska Gymnasiet',
+		url: 'http://www.sydgym.se'
+	},
+	{
+		title: 'Malmö FF',
+		url: 'http://www.mff.se'
+	},
+	{
+		title: 'Google',
+		url: 'http://www.google.se'
+	}
+];
+Template.postsList.helpers({
+	posts: postsData
+});
